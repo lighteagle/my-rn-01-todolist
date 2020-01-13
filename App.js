cf56bb6cae7efd8fed22c5aa075be44ae2d4f33b
lighteagle <sofyan.agus18@gmail.com>
2020-01-13 11:43:03 +0700

    update refactor todo

--- App.js
+++ App.js
@@ -1,43 +1,12 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
 
-import Header from './components/Header';
-import TodoItems from './components/TodoItems';
-import AddTodo from './components/AddTodo';
+import Todo from './components/Todo/Todo';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {text: 'buy coffee', key: '1'},
-    {text: 'create an app', key: '2'},
-    {text: 'play on the switch', key: '3'},
-  ]);
-
-  const pressHandler = key => {
-    setTodos(prevTodos => {
-      return prevTodos.filter(todo => todo.key !== key);
-    });
-  };
-
-  const submitHandler = text => {
-    setTodos(prevTodos => {
-      return [{text: text, key: Math.random().toString()}, ...prevTodos];
-    });
-  };
-
   return (
     <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({item}) => (
-              <TodoItems item={item} pressHandler={pressHandler} />
-            )}
-          />
-        </View>
-      </View>
+      <Todo />
     </View>
   );
 }
